a42980a148afc57e05394a7d0a924eca97b95982
MaskedRedstonerProZ <maskedredstonerproz@gmail.com>
2022-10-25 20:22:03 +0200

    - Added text field state(without viewmodel, for now)

--- app/src/main/java/com/milanradosavac/textpad/feature_text_editing/presentation/screens/main_screen/MainScreen.kt
+++ app/src/main/java/com/milanradosavac/textpad/feature_text_editing/presentation/screens/main_screen/MainScreen.kt
@@ -1,6 +1,5 @@
 package com.milanradosavac.textpad.feature_text_editing.presentation.screens.main_screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.*
@@ -9,7 +8,7 @@ import androidx.compose.material.icons.outlined.Create
 import androidx.compose.material.icons.outlined.DeleteOutline
 import androidx.compose.material.icons.outlined.Redo
 import androidx.compose.material.icons.outlined.Undo
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -48,48 +47,56 @@ fun MainScreen(
             }
         }
         Box {
+
+            var testState by remember {
+                mutableStateOf(
+                    "Lorem ipsum dolor sit amet, " +
+                            "consectetur adipiscing elit. Proin enim, " +
+                            "mattis eget massa quam mattis morbi. " +
+                            "Eget donec vestibulum, varius urna. " +
+                            "Non at parturient etiam egestas tincidunt. " +
+                            "Purus metus, et ut egestas. " +
+                            "Non ullamcorper sem risus scelerisque curabitur. " +
+                            "Sed risus faucibus gravida vel, " +
+                            "urna consequat lectus nec fringilla. " +
+                            "Lacinia fermentum eget faucibus at lorem. " +
+                            "Mauris tellus sit amet," +
+                            "pellentesque. Consectetur et, " +
+                            "at neque faucibus ipsum elit mi dignissim." +
+                            "Pellentesque pellentesque eu massa aliquam. " +
+                            "At dui sit id amet ornare. " +
+                            "Sit quisque odio a id id eu et, " +
+                            "id. Mi, massa at eu, pretium. " +
+                            "Et pellentesque est nibh ac aliquam. " +
+                            "Nunc, venenatis elit, in sed sed amet, " +
+                            "sed feugiat. Et amet consequat ut adipiscing. " +
+                            "Lorem ipsum dolor sit amet, " +
+                            "consectetur adipiscing elit. Proin enim, " +
+                            "mattis eget massa quam mattis morbi. " +
+                            "Eget donec vestibulum, varius urna. " +
+                            "Non at parturient etiam egestas tincidunt. " +
+                            "Purus metus, et ut egestas. " +
+                            "Non ullamcorper sem risus scelerisque curabitur. " +
+                            "Sed risus faucibus gravida vel, " +
+                            "urna consequat lectus nec fringilla. " +
+                            "Lacinia fermentum eget faucibus at lorem. " +
+                            "Mauris tellus sit amet," +
+                            "pellentesque. Consectetur et, " +
+                            "at neque faucibus ipsum elit mi dignissim." +
+                            "Pellentesque pellentesque eu massa aliquam. " +
+                            "At dui sit id amet ornare. " +
+                            "Sit quisque odio a id id eu et, " +
+                            "id. Mi, massa at eu, pretium. " +
+                            "Et pellentesque est nibh ac aliquam. " +
+                            "Nunc, venenatis elit, in sed sed amet, " +
+                            "sed feugiat. Et amet consequat ut adipiscing."
+                )
+            }
             TextField(
-                value = "Lorem ipsum dolor sit amet, " +
-                        "consectetur adipiscing elit. Proin enim, " +
-                        "mattis eget massa quam mattis morbi. " +
-                        "Eget donec vestibulum, varius urna. " +
-                        "Non at parturient etiam egestas tincidunt. " +
-                        "Purus metus, et ut egestas. " +
-                        "Non ullamcorper sem risus scelerisque curabitur. " +
-                        "Sed risus faucibus gravida vel, " +
-                        "urna consequat lectus nec fringilla. " +
-                        "Lacinia fermentum eget faucibus at lorem. " +
-                        "Mauris tellus sit amet," +
-                        "pellentesque. Consectetur et, " +
-                        "at neque faucibus ipsum elit mi dignissim." +
-                        "Pellentesque pellentesque eu massa aliquam. " +
-                        "At dui sit id amet ornare. " +
-                        "Sit quisque odio a id id eu et, " +
-                        "id. Mi, massa at eu, pretium. " +
-                        "Et pellentesque est nibh ac aliquam. " +
-                        "Nunc, venenatis elit, in sed sed amet, " +
-                        "sed feugiat. Et amet consequat ut adipiscing. " +
-                        "Lorem ipsum dolor sit amet, " +
-                        "consectetur adipiscing elit. Proin enim, " +
-                        "mattis eget massa quam mattis morbi. " +
-                        "Eget donec vestibulum, varius urna. " +
-                        "Non at parturient etiam egestas tincidunt. " +
-                        "Purus metus, et ut egestas. " +
-                        "Non ullamcorper sem risus scelerisque curabitur. " +
-                        "Sed risus faucibus gravida vel, " +
-                        "urna consequat lectus nec fringilla. " +
-                        "Lacinia fermentum eget faucibus at lorem. " +
-                        "Mauris tellus sit amet," +
-                        "pellentesque. Consectetur et, " +
-                        "at neque faucibus ipsum elit mi dignissim." +
-                        "Pellentesque pellentesque eu massa aliquam. " +
-                        "At dui sit id amet ornare. " +
-                        "Sit quisque odio a id id eu et, " +
-                        "id. Mi, massa at eu, pretium. " +
-                        "Et pellentesque est nibh ac aliquam. " +
-                        "Nunc, venenatis elit, in sed sed amet, " +
-                        "sed feugiat. Et amet consequat ut adipiscing.",
-                onValueChange = { },
+                value = testState,
+                onValueChange = {
+                    testState = it
+                },
                 singleLine = false,
                 colors = TextFieldDefaults.textFieldColors(
                     unfocusedIndicatorColor = Color.Transparent,
@@ -99,7 +106,8 @@ fun MainScreen(
                     backgroundColor = Color.Transparent
                 ),
                 shape = MaterialTheme.shapes.small.copy(CornerSize(0.dp)),
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
             )
             Row(
                 Modifier
@@ -109,14 +117,23 @@ fun MainScreen(
                 horizontalArrangement = Arrangement.SpaceAround,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Button(onClick = {  }) {
-                    Text(text = stringResource(id = R.string.open), style = MaterialTheme.typography.body1)
+                Button(onClick = { }) {
+                    Text(
+                        text = stringResource(id = R.string.open),
+                        style = MaterialTheme.typography.body1
+                    )
                 }
-                Button(onClick = {  }) {
-                    Text(text = stringResource(id = R.string.close), style = MaterialTheme.typography.body1)
+                Button(onClick = { }) {
+                    Text(
+                        text = stringResource(id = R.string.close),
+                        style = MaterialTheme.typography.body1
+                    )
                 }
-                Button(onClick = {  }) {
-                    Text(text = stringResource(id = R.string.save), style = MaterialTheme.typography.body1)
+                Button(onClick = { }) {
+                    Text(
+                        text = stringResource(id = R.string.save),
+                        style = MaterialTheme.typography.body1
+                    )
                 }
             }
         }
